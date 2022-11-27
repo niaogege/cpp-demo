87ef9e6be9c2028f43376b929b0f4adf55a297e4
chendapeng <pengpeng.chen@ximalaya.com>
2022-11-27 13:53:26 +0800

    feat: add ci.yml

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd build
+
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='cpp1 deploy'
+  githubUrl=git@github.com:niaogege/cpp-demo.git
+else
+  msg='cpp2 from github actions auto deploy'
+  githubUrl=https://niaogege:${GITHUB_TOKEN}@github.com:niaogege/cpp-demo.git
+  git config --global user.name "niaogege"
+  git config --global user.email "291003932@qq.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:cpp-gh-pages # 推送到github gh-pages分支
